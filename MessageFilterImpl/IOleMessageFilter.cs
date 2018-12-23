c7e32574010937e615b03c9dcb7ca1ec8f3e3f85
Haroon Khan <hkhan@healthexpense.com>
2018-12-23 12:19:57 -0800

    Add Project files for Visual Studio Automation

---
+++ MessageFilterImpl/IOleMessageFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MessageFilterImpl
+{
+  [ComImport]
+  [Guid("00000016-0000-0000-C000-000000000046")]
+  [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+  public interface IOleMessageFilter
+  {
+    [PreserveSig]
+    int HandleInComingCall(int dwCallType, IntPtr hTaskCaller,
+                           int dwTickCount, IntPtr lpInterfaceInfo);
+    [PreserveSig]
+    int RetryRejectedCall(IntPtr hTaskCallee, int dwTickCout, int dwRejectType);
+
+    [PreserveSig]
+    int MessagePending(IntPtr hTaskCallee, int dwTickCount, int dwPendingType);
+  }
+}
---
+++ MessageFilterImpl/MessageFilter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MessageFilterImpl
+{
+  public class MessageFilter : IOleMessageFilter
+  {
+    [DllImport("Ole32.dll")]
+    private static extern int CoRegisterMessageFilter(IOleMessageFilter hTaskCallee, out IOleMessageFilter oldFilter);
+
+
+
+    #region IOleMessageFilter Members
+
+    int IOleMessageFilter.HandleInComingCall(int dwCallType, IntPtr hTaskCaller, int dwTickCount, IntPtr lpInterfaceInfo)
+    {
+      return 0;
+    }
+
+    int IOleMessageFilter.RetryRejectedCall(IntPtr hTaskCallee, int dwTickCout, int dwRejectType)
+    {
+      if (dwRejectType==2){
+        return 99;
+      }
+      return -1;
+    }
+
+    int IOleMessageFilter.MessagePending(IntPtr hTaskCallee, int dwTickCount, int dwPendingType)
+    {
+      return 2;
+    }
+
+    #endregion
+
+
+    public static void Register()
+    {
+      var newFilter = new MessageFilter() as IOleMessageFilter;
+      var oldFilter = null as IOleMessageFilter;
+      CoRegisterMessageFilter(newFilter, out oldFilter);
+    }
+
+    public static void Revoke()
+    {
+      var oldFilter = null as IOleMessageFilter;
+      CoRegisterMessageFilter(null, out oldFilter);
+    }
+  }
+}
